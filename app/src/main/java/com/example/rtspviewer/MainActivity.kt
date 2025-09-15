0de090183a9dd72a0db68b00e4bf5a4136d7cd2f
pigkings <vrtest9528@outlook.com>
2025-09-16 03:15:45 +0800

    new

--- app/src/main/java/com/example/rtspviewer/MainActivity.kt
+++ app/src/main/java/com/example/rtspviewer/MainActivity.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import android.view.SurfaceHolder
-import android.view.SurfaceView
+import org.videolan.libvlc.util.VLCVideoLayout
 import androidx.appcompat.app.AppCompatActivity
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -14,10 +13,10 @@ import org.videolan.libvlc.Media
 import org.videolan.libvlc.MediaPlayer
 import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
+class MainActivity : AppCompatActivity() {
     private lateinit var libVLC: LibVLC
     private lateinit var mediaPlayer: MediaPlayer
-    private lateinit var surfaceView: SurfaceView
+    private lateinit var videoLayout: VLCVideoLayout
     private lateinit var handler: Handler
     private lateinit var httpClient: OkHttpClient
 
@@ -29,8 +28,9 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        surfaceView = findViewById(R.id.surfaceView)
-        surfaceView.holder.addCallback(this)
+        videoLayout = findViewById(R.id.videoLayout)
+        // VLCVideoLayout 可能不需要 SurfaceHolder 回调
+        // 直接使用 attachViews 方法
 
         handler = Handler(Looper.getMainLooper())
         httpClient = OkHttpClient.Builder()
@@ -47,12 +47,35 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
         libVLC = LibVLC(this, options)
     }
 
-    override fun surfaceCreated(holder: SurfaceHolder) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        videoLayout = findViewById(R.id.videoLayout)
+
+        handler = Handler(Looper.getMainLooper())
+        httpClient = OkHttpClient.Builder()
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.SECONDS)
+            .build()
+
+        val options = ArrayList<String>().apply {
+            add("--aout=opensles")
+            add("--avcodec-codec=h264")
+            add("--file-logging")
+            add("--logfile=vlc-log.txt")
+        }
+        libVLC = LibVLC(this, options)
+
+        // 初始化媒体播放器并开始播放
+        initializeMediaPlayer()
+    }
+
+    private fun initializeMediaPlayer() {
         mediaPlayer = MediaPlayer(libVLC).apply {
             setVideoTrackEnabled(true)
             setVideoScale(MediaPlayer.ScaleType.SURFACE_BEST_FIT)
-            // 使用正确的 attachViews 方法，可能需要 SurfaceView 而不是 SurfaceHolder
-            attachViews(surfaceView, null, false, false)
+            attachViews(videoLayout, null, false, false)
         }
 
         val media = Media(libVLC, android.net.Uri.parse(RTSP_URL)).apply {
@@ -91,13 +114,13 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
         }.start()
     }
 
-    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        mediaPlayer.setVideoScale(MediaPlayer.ScaleType.SURFACE_BEST_FIT)
-    }
-
-    override fun surfaceDestroyed(holder: SurfaceHolder) {
+    override fun onDestroy() {
+        super.onDestroy()
         mediaPlayer.stop()
         mediaPlayer.detachViews()
+        mediaPlayer.release()
+        libVLC.release()
+        handler.removeCallbacksAndMessages(null)
     }
 
     override fun onDestroy() {
